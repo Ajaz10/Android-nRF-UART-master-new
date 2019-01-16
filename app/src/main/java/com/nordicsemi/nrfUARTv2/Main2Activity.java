be585ca0330afeef76a020988d507f36c07dbf91
Ajaz10 <ajazf09@gmail.com>
2019-01-16 23:12:29 +0530

    Initial commit

---
+++ app/src/main/java/com/nordicsemi/nrfUARTv2/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.nordicsemi.nrfUARTv2;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button btn = (Button)findViewById(R.id.open_activity_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this, MainActivity.class));
+            }
+        });
+    }
+}
